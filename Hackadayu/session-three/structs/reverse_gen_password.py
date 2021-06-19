feb6b42383b2efd528780f215699d00566e0fb6e
OaHau <oleandrehauge@gmail.com>
2021-06-19 14:14:38 +0200

    fixing syntax and readability

--- Hackadayu/session-three/structs/reverse_gen_password.py
+++ Hackadayu/session-three/structs/reverse_gen_password.py
@@ -1,29 +1,27 @@
-import binascii
+from binascii import hexlify
 
 
-def tohex(val, nbits):
+def tohex(val: int, nbits: int) -> str:
   return hex((val + (1 << nbits)) % (1 << nbits))
 
 
-def gen_password(k, u):
-    result = ''
-    key = k
-    u = binascii.hexlify(u)
-    y = str(u, 'ascii')
-    username = [y[i:i + 2] for i in range(0, len(y), 2)]
+def gen_password(key: int, username: bytes):
+    result: str = ''
+    username_hex: bytes = hexlify(username)
+    username_ascii = str(username_hex, 'ascii')
+    username_split: list = [username_ascii[i:i + 2] for i in range(0, len(username_ascii), 2)]
 
-    for char in username:
-        result = result + '\\'
+    for char in username_split:
+        result += '\\'
         char_integer = int(char, 16)
-        XOR = hex(char_integer ^ key)
+        XOR: str = hex(char_integer ^ key)
         AL = int(XOR[-2:], 16)
-        sub_AL = AL - 19
-        AL_hex = tohex(sub_AL, 8)  # Last byte.
-        result = result + AL_hex
+        sub_AL: int = AL - 19
+        AL_hex: str = tohex(sub_AL, 8)  # Last byte.
+        result += AL_hex
     return result.replace('0', '')
 
 
 if __name__ == '__main__':
     # Provide key as an integer and username in the b'yourstringhere' format.
     print('Use: ' + gen_password(12345678, b'AAAABBBB') + ' as password for the supplied key and username.')
-
