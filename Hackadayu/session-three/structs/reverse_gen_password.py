2c196a5034409b9187fa5d97beb8b1b03fe61504
OaHau <oleandrehauge@gmail.com>
2021-06-19 14:19:52 +0200

    username_split: list = more spesific: username_split: list[str]

--- Hackadayu/session-three/structs/reverse_gen_password.py
+++ Hackadayu/session-three/structs/reverse_gen_password.py
@@ -9,7 +9,7 @@ def gen_password(key: int, username: bytes) -> str:
     result: str = ''
     username_hex: bytes = hexlify(username)
     username_ascii = str(username_hex, 'ascii')
-    username_split: list = [username_ascii[i:i + 2] for i in range(0, len(username_ascii), 2)]
+    username_split: list[str] = [username_ascii[i:i + 2] for i in range(0, len(username_ascii), 2)]
 
     for char in username_split:
         result += '\\'
